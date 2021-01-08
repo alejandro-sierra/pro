d83469f4dae2cc3f2c2d9b1e1e8218b4183b87c4
Alejandro Sierra <alejandrosierraballesta@gmail.com>
2021-01-08 19:32:54 +0100

    creado ejercicio 06 ede

---
+++ EDE04EJ06/src/ede04ej06/Jugador.java
@@ -0,0 +1,25 @@
+package ede04ej06;
+
+public class Jugador {
+    private String usuario;
+    private String nombre;
+    private int nivel;
+    private int experiencia;
+    private int contador;
+    
+//contructor vacio
+    public Jugador(){
+    }
+    
+    public void ganarExperiencia(){
+    }
+
+    public void subirNivel(){
+    }
+
+    public void tieneXPparaSigNivel(){
+    }
+
+    public String getRecompensasSubirNivel(){
+    }
+}
